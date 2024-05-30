7a4c7ec20644f3a0531b25fad156d05b88a6072b
sudipnext <parajulisudip123@outlook.com>
2024-05-30 14:15:49 +0545

    Pages Added

---
+++ my-app/app/manage_store/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const manage_store = () => {
+  return (
+    <div>manage_store</div>
+  )
+}
+
+export default manage_store
---
+++ my-app/app/roles_and_access/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const roles_and_access = () => {
+  return (
+    <div>roles_and_access</div>
+  )
+}
+
+export default roles_and_access
