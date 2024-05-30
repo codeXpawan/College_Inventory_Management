947d0e93c24cf821e5e60db8ea367aba14bdb49c
codeXpawan <shahpawan56@gmail.com>
2024-05-30 16:47:03 +0545

    search bar fully completed

--- my-app/components/custom_ui/search_bar.tsx
+++ my-app/components/custom_ui/search_bar.tsx
@@ -2,14 +2,14 @@ import Image from "next/image"
 import { Input } from "../ui/input"
 export default function SerachBar(){
     return (
-        <div className="flex justify-center flex-col w-[574px] h-[44px] border rounded-[4px] border-[#1570EF]">
-            <div className="flex ml-2 text-[#1570EF] text-base"><Image 
+        <div className="flex text-[#1570EF] justify-center flex-col w-[574px] h-[44px] border rounded-[4px] border-[#1570EF]">
+            <div className="flex ml-2  text-base"><Image 
                 src="/Search.svg"
                 width={24}
                 height={24}
                 alt="Search Bar"
             />
-            <Input placeholder="Search product, supplier, order"/></div>
+            <Input className=" placeholder:text-[#1570EF] focus-visible:ring-[#1570EF]" placeholder="Search product, supplier, order"/></div>
         </div>
     )
 }
