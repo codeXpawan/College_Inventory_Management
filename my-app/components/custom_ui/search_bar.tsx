c50b37e2490c6442d758244fc1778def775d3c86
codeXpawan <shahpawan56@gmail.com>
2024-05-30 15:54:49 +0545

    Search Bar Component added

---
+++ my-app/components/custom_ui/search_bar.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+export default function SerachBar(){
+    return (
+        <div className="flex justify-center flex-col w-[574px] h-[44px] border rounded-[4px] border-[#1570EF]">
+            <div className="flex ml-2 text-[#1570EF] text-base"><Image 
+                src="/Search.svg"
+                width={24}
+                height={24}
+                alt="Search Bar"
+            />Search product, supplier, order</div>
+        </div>
+    )
+}
