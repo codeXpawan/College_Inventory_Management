e8e76fe2e5e7a8ffec4afe94e8a58a5553a6518a
Suwashshrestha <suwashshrestha5536@gmail.com>
2024-06-08 12:46:43 +0545

    sidebar change

---
+++ my-app/components/ui/pageTitle.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import { cn } from "@/lib/utils";
+import { ChevronRight } from "lucide-react";
+type Props = {
+    title: string;
+    className?: string;
+}
+export default function PageTitle({title, className }: Props) {
+    return <h1 className={cn("text-2*1 font-semibold flex ", className)}>
+        <ChevronRight />
+        {title}
+        </h1>
+}
