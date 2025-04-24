0a72238fc542905df6af9d2005eeb3e3a38264f1
codeXpawan <shahpawan56@gmail.com>
2025-04-24 13:48:02 +0545

    Updated the changes

--- my-app/components/custom_ui/Notification.tsx
+++
@@ -1,9 +0,0 @@
-import { Bell } from "lucide-react";
-
-export default function Notification() {
-  return (
-    <div className="relative">
-      <Bell className="h-6 w-6 pt-2" />
-    </div>
-  );
-}
